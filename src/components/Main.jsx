727bfec795fcba490a09519f403d533854b1c78d
hoangson15102000 <nguyenhoangsonasus@gmail.com>
2022-12-05 15:59:12 +0700

    finish commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,6 +29,7 @@ const Main = () => {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
+        <span></span>
         <div className="absolute top-[20%] w-full p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl mb-2 font-bold">
             {movie?.title}
